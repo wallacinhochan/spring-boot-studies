19213e40d4c7c427893121f3d4d757dd7f04c147
Wallace Gabriel <wgsl@cin.ufpe.br>
2025-11-01 14:54:42 -0300

    intital commit of the project

---
+++ spring-boot-trincheiras/src/main/java/studies/wallace/SpringBootTrincheirasApplication.java
@@ -0,0 +1,13 @@
+package studies.wallace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTrincheirasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTrincheirasApplication.class, args);
+	}
+
+}
---
+++ spring-boot-trincheiras/src/test/java/studies/wallace/SpringBootTrincheirasApplicationTests.java
@@ -0,0 +1,13 @@
+package studies.wallace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTrincheirasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
